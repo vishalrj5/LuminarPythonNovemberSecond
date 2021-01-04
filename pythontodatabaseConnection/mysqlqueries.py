57131f0af7a7cdbb1b6f683c774c7c89e2d9ffe6
vishalrj5 <vishalrj5@gmail.com>
2021-01-05 00:45:13 +0530

    MySql started

---
+++ pythontodatabaseConnection/mysqlqueries.py
@@ -0,0 +1,13 @@
+#crud operation
+#c -> create
+#r -> read
+#u -> update
+#d -> delete
+
+
+#collegeManagementSystem
+#1)create database for project
+#listing all databases
+#ssudo mysql
+#sq>create database databasename;
+#tables
