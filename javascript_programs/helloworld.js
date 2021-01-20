a50205afcbfa150479848589892dcd36d2df685c
vishalrj5 <vishalrj5@gmail.com>
2021-01-20 09:57:06 +0530

    javascript

---
+++ javascript_programs/helloworld.js
@@ -0,0 +1,25 @@
+//
+var companyname="luminar Technolab";
+// var , let,const
+// print()
+//printf() cout>>
+// console.log()
+console.log(companyname);
+// there is no special method for reading from console
+var num=10;//int  //number
+var num2=10.5;//float  //number
+var isTrue=false;//boolean
+var name1="test"//string
+
+console.log(typeof(num))
+console.log(typeof(num2))
+console.log(typeof(isTrue))
+console.log(typeof(name1))
+
+//number
+//boolean
+//string
+//undefined
+
+//bootstrap (Luminar,resume)
+//javascript
